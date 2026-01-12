d01b13fe82b9ee077606c59ddbbfd6f8ad061ffc
Pixeleru <pixeleru@proton.me>
2026-01-11 21:29:10 -0600

    Add Microsoft.NET.ILLink.Tasks package and update project configuration
    - Integrated Microsoft.NET.ILLink.Tasks version 10.0.0 for IL linking.
    - Updated project.assets.json and project.nuget.cache to include new package dependencies.
    - Enhanced .gitignore to exclude common build folders and user-specific files.

--- obj/Release/net10.0/linux-x64/BoxyTube.AssemblyInfo.cs
+++ obj/Release/net10.0/linux-x64/BoxyTube.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoxyTube")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61b623c3a63805c4cd2d888fb50bf6381a09ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoxyTube")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoxyTube")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
